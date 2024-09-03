460db6c1d4a5b49bdbac5222e791b4628c73bf9a
DELL <nazwaamanda003@mail.com>
2024-09-03 20:38:38 +0700

    Starting project

---
+++ src/variabel.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 // assigning firts value to varVariabel
+    println("Pemberian nilai pada varVariabel = "+varVariabel)
+    varVariabel = 78 // assigning second value to varVariabel
+    println("Pemberian Nilai Kedua pada varVariabel = "+ varVariabel)
+    val phi = 3.14
+    println(phi)
+}
