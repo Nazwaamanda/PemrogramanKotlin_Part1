be4ad14634ee065cf063b3bd7086dce010f8588c
DELL <nazwaamanda003@mail.com>
2024-09-03 19:45:41 +0700

    Starting project

---
+++ src/dynamic.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh Dynamic variabel = "+dynamicVar)
+}
---
+++ src/literal.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+
+    println("intLiteral = "+ intLiteral)
+    println("doubleLiteral = "+ doubleLiteral)
+    println("StringLiteral = "+ stringLiteral)
+    println("charLiteral = "+charLiteral)
+    println("boolLiteral = "+boolLiteral)
+}
